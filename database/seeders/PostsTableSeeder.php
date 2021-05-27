8b25d8cce22885319934837490adc472c94583f1
ThomasD77 <thomas.demeulenaere@hotmail.com>
2021-05-27 20:31:22 +0200

    first commit

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('posts')->insert([
+            'title'=>'Thomas',
+            'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
